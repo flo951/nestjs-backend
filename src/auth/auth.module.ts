d7ebf35678315bd16bdc5c82f3582cf2c30cec57
Florian Görlich <florian.goerlich95@gmail.com>
2023-11-04 14:09:07 +0100

    Initial commit, setup prisma and docker

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Global, Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Global()
+@Module({
+  imports: [PrismaModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable({})
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  signup() {
+    return 'hello world';
+  }
+}
